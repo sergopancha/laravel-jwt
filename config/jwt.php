01a23da3aff8ebd56b9a9a4670f8932be429ad64
tulazis@yandex.ru <tulazis@yandex.ru>
2024-03-09 21:44:03 +0300

    через php artisan jwt:secret сгенерировали jwt secret key, он сохранился в .evn

--- config/jwt.php
+++ config/jwt.php
@@ -7,6 +7,9 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ *
+ * Генерация секретного ключа:
+ * php artisan jwt:secret
  */
 
 return [
